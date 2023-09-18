c8fd0c705b6da44c82be8e9b5f9364866567ab76
jtate <jtate.cc@outlook.com>
2023-09-18 14:49:41 +0100

    ticket 3 completed

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,12 +1,17 @@
 function countNumberOfNinesInNumber(number) {
   let count = 0;
-
-  for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
-    if ("9" === digit) {
+  let numStr = number.toString();
+  for (let i = 0; i < numStr.length; i++) {
+    const digit = numStr[i];
+    if (digit === "9") {
       count++;
     }
   }
 
   return count;
 }
+
+console.log(countNumberOfNinesInNumber(99.999))       // 5 
+console.log(countNumberOfNinesInNumber(9))          // 1 
+
+// bug solved as input was a number but operations required string inputs.
